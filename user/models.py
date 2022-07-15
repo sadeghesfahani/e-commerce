08bdfb988e9426e7ffde74d1131313ce0a9cbcee
Sina <sadeghesfahani.sina@gmail.com>
2022-07-15 17:20:02 +0430

    feat(user): user model created

--- user/models.py
+++ user/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class User(AbstractUser):
+    address = models.TextField(null=True, blank=True)
