f73ed409a7a07bdbc169e55c4fbb1443ad35cc24
Sina <sadeghesfahani.sina@gmail.com>
2022-07-16 08:16:35 +0430

    feat(store): models added to admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from store.models import Category, Order, Basket, Coupon, Product, Address
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Order)
+admin.site.register(Basket)
+admin.site.register(Coupon)
+admin.site.register(Product)
+admin.site.register(Address)
