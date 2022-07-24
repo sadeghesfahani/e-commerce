af26893f81e1f5ea1250e5f024c101f98a9dd8d8
Sina <sadeghesfahani.sina@gmail.com>
2022-07-24 20:16:23 +0430

    feat(user): phone and email added to user data structure

--- user/models.py
+++ user/models.py
@@ -5,3 +5,4 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     address = models.TextField(null=True, blank=True)
+    phone = models.CharField(max_length=20, null=True, blank=True)
