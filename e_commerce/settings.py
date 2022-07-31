8599c669a6fffe379919f90e831846a18168ef4f
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-07-31 11:32:36 -0700

    Added the static folder and also added some env input

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
+from os.path import normpath
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!&kkbe3fwd#okq&g#sl8q*vj8wla!arvu*gez+zo)fg88r$pg$'
+SECRET_KEY = os.getenv(
+    "DJANGO_SECRET_KEY", 'django-insecure-!&kkbe3fwd#okq&g#sl8q*vj8wla!arvu*gez+zo)fg88r$pg$')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", False)
 
 ALLOWED_HOSTS = ["*"]
 
@@ -58,7 +61,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'e_commerce.urls'
 
-LOCAL_APP =[
+LOCAL_APP = [
     'user',
     'store'
 ]
@@ -71,8 +74,7 @@ REST_FRAMEWORK = {
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -143,6 +145,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = normpath(f"{Path(__file__).parent.resolve()}/../static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
