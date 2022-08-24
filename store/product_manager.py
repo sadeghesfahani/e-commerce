7f9ef896bf93cf430e5eb71ea5dc3efdc41cfad1
Sina <sadeghesfahani.sina@gmail.com>
2022-08-24 19:42:27 +0430

    fix(brand): editing brand fixed

--- store/product_manager.py
+++ store/product_manager.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from rest_framework.response import Response
 
 from store.data_structures import ProductDataStructure
-from store.models import Category
+from store.models import Category, Brand
 from store.serializers import ProductSerializer
 
 
@@ -17,6 +17,8 @@ class ProductManager:
             self.product.__dict__.update(**structured_product.__dict__)
             if structured_product.__dict__.get('category'):
                 self.product.category = get_object_or_404(Category, id=structured_product.__dict__.get("category").id)
+            if structured_product.__dict__.get('brand'):
+                self.product.brand = get_object_or_404(Brand, id=structured_product.__dict__.get("brand").id)
             self.product.save()
             return Response(ProductSerializer(self.product, many=False, read_only=True).data, status=status.HTTP_200_OK)
         except Exception as ex:
